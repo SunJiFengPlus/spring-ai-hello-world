43408da9f25bbaaef8509fdba7abee95116590c5
sunjifeng <jifeng.sun@outlook.com>
2025-04-11 15:51:53 +0800

    流式Agent

--- agent/src/main/java/com/test/agent/AgentApplication.java
+++ agent/src/main/java/com/test/agent/AgentApplication.java
@@ -1,12 +1,12 @@
 package com.test.agent;
 
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.tool.ToolCallbackProvider;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Flux;
 
 @SpringBootApplication
 public class AgentApplication {
@@ -16,22 +16,20 @@ public class AgentApplication {
     }
 
     @Bean
-    public CommandLineRunner predefinedQuestions(ChatClient.Builder chatClientBuilder, ToolCallbackProvider tools,
-        ConfigurableApplicationContext context) {
-        
+    public CommandLineRunner predefinedQuestions(ChatClient.Builder chatClientBuilder, ConfigurableApplicationContext context) {
         return args -> {
-            // 构建ChatClient并注入MCP工具
-            var chatClient = chatClientBuilder
-                .defaultTools(tools)
-                .build();
-
+            var chatClient = chatClientBuilder.build();
             // 定义用户输入
-            String userInput = "北京的天气如何？";
+            String userInput = "你是谁";
             // 打印问题
             System.out.println("\n>>> QUESTION: " + userInput);
             // 调用LLM并打印响应
-            System.out.println("\n>>> ASSISTANT: " +
-                chatClient.prompt(userInput).call().content());
+            System.out.println("\n>>> ASSISTANT: ");
+            chatClient.prompt(userInput)
+                .stream()
+                .content()
+                .doOnNext(System.out::print)
+                .blockLast();
 
             // 关闭应用上下文
             context.close();
